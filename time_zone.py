8df77f95ad37703c8e10c652dc9110ddd8c89d89
Rohan Singh <rohansinghrajput.dev@gmail.com>
2024-05-15 10:19:46 +0530

    add database

--- time_zone.py
+++ time_zone.py
@@ -1,5 +1,4 @@
 import datetime as dt
-import json
 from pytz import timezone  # pip install pytz
 # from dotenv import load_dotenv  # pip install python-dotenv
 import os
@@ -9,6 +8,12 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions
 # from discord.utils import get
 from keep_alive import keep_alive
+# pip install SQLAlchemy
+from sqlalchemy import create_engine, ForeignKey, Column, String
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
 keep_alive()
 
 
@@ -17,28 +22,58 @@ TOKEN = os.environ.get('TOKEN')
 
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
-# Load channel data from JSON
+# SQLAlchemy setup
+Base = declarative_base()
+class ChannelData(Base):
+    __tablename__ = 'channel_data'
+    
+    person = Column("person", String)
+    channel_id = Column("channel_id", String, primary_key=True)
+    timezone = Column("timezone", String)
+    
+    def __init__(self, person, channel_id, timezone):
+        self.person = person
+        self.channel_id = channel_id
+        self.timezone = timezone
+        
+    def __repr__(self):
+        return f"<Person: '{self.person}', Channel_id: '{self.channel_id}'"
+
+engine = create_engine('sqlite:///channel_data.db', echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+
+# Load channel data from SQLite database
 def load_channel_data():
-    with open('channels.json') as f:
-        return json.load(f)
+    session = Session()
+    return session.query(ChannelData).all()
 
-# Save channel data to JSON
-def save_channel_data(channel_data):
-    with open('channels.json', 'w') as f:
-        json.dump(channel_data, f, indent=4)
+# # Save channel data to SQLite database
+# def save_channel_data(channel_data):
+#     session = Session()
+#     session.bulk_save_objects(channel_data)
+#     session.commit()
 
+@bot.command()
+@has_permissions(administrator=True)
+async def reset_time(ctx):
+    """
+    Reset the time (rename the channel).
+    """
+    channel_data = load_channel_data()
+    for data in channel_data:
+        channel = bot.get_channel(int(data.channel_id))
+        await channel.edit(name=await get_time_in_timezone(data.timezone, data.person))
+    await ctx.send("Time reset for all channels.")
 
 async def rename_channels():
     await bot.wait_until_ready()
     while not bot.is_closed():
         channel_data = load_channel_data()
-        for person, data in channel_data.items():
-            channel_id = data['channel_id']
-            timezone_name = data['timezone']
-            channel = bot.get_channel(int(channel_id))
-            await channel.edit(name=await get_time_in_timezone(timezone_name, person))
+        for data in channel_data:
+            channel = bot.get_channel(int(data.channel_id))
+            await channel.edit(name=await get_time_in_timezone(data.timezone, data.person))
         await asyncio.sleep(600)  # Update every 10 minutes
-
 
 async def get_time_in_timezone(timezone_name, person):
     now_time = dt.datetime.now(timezone(timezone_name)).strftime("%I:%M %p")
@@ -66,85 +101,56 @@ async def createvc(ctx):
     await guild.create_voice_channel(name='new-vc', overwrites=overwrites)
 
 
+
 @bot.command()
-@has_permissions(administrator=True)
+@commands.has_permissions(administrator=True)
 async def add_channel_data(ctx, person, channel_id, timezone):
     """
-    Add channel data to channels.json if the channel_id is not already present.
-
-    Usage: /add_channel_data <person> <channel_id> <timezone>
+    Add channel data to SQLite database.
     """
-    channel_data = load_channel_data()
-    for existing_person, data in channel_data.items():
-        if data['channel_id'] == channel_id:
-            await ctx.send(f"The channel ID {channel_id} is already associated with {existing_person}.")
-            return
-
-    channel_data[person] = {
-        "channel_id": channel_id,
-        "timezone": timezone
-    }
-
-    save_channel_data(channel_data)
+    session = Session()
+    channel_data = ChannelData(person=person, channel_id=channel_id, timezone=timezone)
+    session.add(channel_data)
+    session.commit()
     await ctx.send(f"Channel data added for {person}.")
 
 @bot.command()
-@has_permissions(administrator=True)
+@commands.has_permissions(administrator=True)
 async def update_channel_data(ctx, person, channel_id, timezone):
     """
-    Update channel data in channels.json.
-
-    Usage: /update_channel_data <person> <channel_id> <timezone>
+    Update channel data in SQLite database.
     """
-    channel_data = load_channel_data()
-    if person not in channel_data:
+    session = Session()
+    channel_data = session.query(ChannelData).filter_by(person=person).first()
+    if channel_data:
+        channel_data.channel_id = channel_id
+        channel_data.timezone = timezone
+        session.commit()
+        await ctx.send(f"Channel data updated for {person}.")
+    else:
         await ctx.send(f"{person} does not exist in the data. Use /add_channel_data instead.")
-        return
-
-    channel_data[person] = {
-        "channel_id": channel_id,
-        "timezone": timezone
-    }
-
-    save_channel_data(channel_data)
-    await ctx.send(f"Channel data updated for {person}.")
 
 @bot.command()
-@has_permissions(administrator=True)
+@commands.has_permissions(administrator=True)
 async def remove_channel_data(ctx, person):
     """
-    Remove channel data from channels.json.
-
-    Usage: /remove_channel_data <person>
+    Remove channel data from SQLite database.
     """
-    channel_data = load_channel_data()
-    if person not in channel_data:
+    session = Session()
+    channel_data = session.query(ChannelData).filter_by(person=person).first()
+    if channel_data:
+        session.delete(channel_data)
+        session.commit()
+        await ctx.send(f"Channel data removed for {person}.")
+    else:
         await ctx.send(f"{person} does not exist in the data.")
-        return
-
-    del channel_data[person]
-    save_channel_data(channel_data)
-    await ctx.send(f"Channel data removed for {person}.")
-
-
-@bot.command()
-async def reset_time(ctx):
-    """
-    Reset the time (rename the channel).
-    """
-    channel_data = load_channel_data()
-    for person, data in channel_data.items():
-        channel_id = data['channel_id']
-        timezone_name = data['timezone']
-        channel = bot.get_channel(int(channel_id))
-        await channel.edit(name=await get_time_in_timezone(timezone_name, person))
-    await ctx.send("Time reset for all channels.")
     
     
 @bot.command()
 @has_permissions(administrator=True)
-async def test(ctx, msg):
-    await ctx.send(f'{msg}, {ctx.author.mention}!')
+async def test(ctx):
+    channel_data = load_channel_data()
+    await ctx.send(f'{channel_data}, {ctx.author.mention}!')
 
 
 if __name__ == '__main__':
